29c927c84076773fd280a5ddc97c981625af4ebf
Naymul Islam <naymul504@gmail.com>
2024-10-19 22:03:49 +0600

    tokenizer logic added
    Signed-off-by: Naymul Islam <naymul504@gmail.com>

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -9,13 +9,30 @@ class TrainTokeniers():
     # train bpe trainer 
     def train_bpe(self, files, special_tokens, vocab_size, min_freq):
         tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
-        trainer = BpeTrainer(special_tokens = special_tokens, vocab_size = vocab_size, min_freq = min_frquency)
+        trainer = BpeTrainer(special_tokens = special_tokens, vocab_size = vocab_size, min_frequency = min_freq)
         tokenizer.train(files, trainer)
         return tokenizer
     # https://huggingface.co/docs/tokenizers/en/api/models#tokenizers.models.Unigram
     # https://huggingface.co/docs/tokenizers/en/api/trainers#tokenizers.trainers.WordLevelTrainer
-    def train_unigram(self, files, special_tokens, vocab_size, min_freq):
-        pass
+    def train_unigram(self, files, special_tokens, vocab_size):
+        tokenizer = Tokenizer(Unigram())
+        trainer = UnigramTrainer(special_tokens, vocab_size = vocab_size)
+      
+        tokenizer.train(files, trainer)
+        return tokenizer
+
+    def train_wordlevel(self, files, special_tokens, vocab_size, min_freq):
+
+        tokenizer = Tokenizer(WordLevel(unk_token=["UNK"]))
+        trainer = WordLevelTrainer(special_tokens, vocab_size=vocab_size, min_frquency=min_freq)
+        tokenizer.train(files=files, trainer=trainer)
+        return tokenizer
+    
+    def train_wordpiece(self, files, special_tokens, vocab_size, min_freq):
+        tokenizer = Tokenizer(WordPiece(unk_token=["UNK"]))
+        trainer = WordPieceTrainer(special_tokens=special_tokens, vocab_size= vocab_size, min_frequency= min_freq)
+        tokenizer.train(files=files, trainer=trainer)
+        return tokenizer
 
 
 

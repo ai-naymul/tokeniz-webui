c74780309908991d38d2c1d133c449e3fbc8db84
Naymul Islam <naymul504@gmail.com>
2024-10-20 14:15:37 +0600

    add loading for tokenizer
    Signed-off-by: Naymul Islam <naymul504@gmail.com>

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,8 +1,10 @@
 from tokenizers import Tokenizer
 from tokenizers.models import BPE, Unigram, WordLevel, WordPiece
-from tokenizers.trainers import BpreTrainer, UnigramTrainer, WordLevelTrainer, WordPieceTrainer
+from tokenizers.trainers import BpeTrainer, UnigramTrainer, WordLevelTrainer, WordPieceTrainer
 from transformers import PreTrainedTokenizerFast
 
+
+## https://www.perplexity.ai/search/i-want-to-build-a-webui-based-_MpcRIypRpqp2h1fld_NEw
 class TrainTokeniers():
     def __init__(self):
         pass
@@ -12,12 +14,12 @@ class TrainTokeniers():
         trainer = BpeTrainer(special_tokens = special_tokens, vocab_size = vocab_size, min_frequency = min_freq)
         tokenizer.train(files, trainer)
         return tokenizer
+    
     # https://huggingface.co/docs/tokenizers/en/api/models#tokenizers.models.Unigram
     # https://huggingface.co/docs/tokenizers/en/api/trainers#tokenizers.trainers.WordLevelTrainer
     def train_unigram(self, files, special_tokens, vocab_size):
         tokenizer = Tokenizer(Unigram())
         trainer = UnigramTrainer(special_tokens, vocab_size = vocab_size)
-      
         tokenizer.train(files, trainer)
         return tokenizer
 
@@ -33,6 +35,11 @@ class TrainTokeniers():
         trainer = WordPieceTrainer(special_tokens=special_tokens, vocab_size= vocab_size, min_frequency= min_freq)
         tokenizer.train(files=files, trainer=trainer)
         return tokenizer
+
+    # loading the tokenizer for uses
+    def load_tokenizer(json_file):
+        fast_tokenizer = PreTrainedTokenizerFast(json_file)
+        return fast_tokenizer
 
 
 
